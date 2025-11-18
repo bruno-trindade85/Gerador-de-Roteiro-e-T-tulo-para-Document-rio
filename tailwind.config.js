7536ae1cb84c09be51a9ca5d8b33432eccf40516
bruno-trindade85 <brunocmt@gmail.com>
2025-11-17 22:22:47 -0300

    feat: Migrate UI to Tailwind CSS
    Integrates Tailwind CSS into the project to manage styling and replace custom CSS. This change also configures Tailwind's theme to use the 'Inter' font.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,7 @@ export default {
   ],
   darkMode: 'class',
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
